abd2e1fb44e91ce3684e5254227e4fb6568468eb
Anurag Hakeem <anuraghakeem@gmail.com>
2019-07-31 01:06:47 +0530

    Added redcucer and action for fetch api

--- typescript-rick-and-morty-episodes/src/App.tsx
+++ typescript-rick-and-morty-episodes/src/App.tsx
@@ -3,11 +3,45 @@ import React from 'react'
 import {Store} from './Store'
 
 export default function App():JSX.Element {
-  const store= React.useContext(Store)
+  const {state,dispatch}= React.useContext(Store)
+
+  React.useEffect(()=>{
+    state.episodes.length===0 && fetchDataAction()
+  })
+
+  const fetchDataAction=async ()=>{
+    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data= await fetch(URL)
+    const dataJSON= await data.json()
+    return dispatch({
+      type:'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+  console.log(state)
   return (
     <React.Fragment>
-      {console.log(Store)}
-      <h1>Rick and Morty</h1>     
+      <h1>Rick and Morty</h1>
+      <p>Which is your favorite episode?</p>
+      <section>
+        {state.episodes.map((episode:any)=>{
+          return(
+            <section key={episode.id}>
+              {/* {console.log(episode)} */}
+              {/* <img src={episode.image.medium} alt={`Rick and Morty ${episode.name}`} /> */}
+              <div>
+                {episode.type}
+              </div>
+              <div>
+                {episode.name}
+              </div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          )
+        })}
+      </section>     
     </React.Fragment>
   )
 }
